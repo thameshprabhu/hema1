25e9aadb47834e6bf65a0e5a9d7d2e8ef18c1b40
thameshprabhu <30828812+thameshprabhu@users.noreply.github.com>
2018-03-16 14:02:59 +0530

    Create hol.c

---
+++ hol.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+int main()
+{
+  char s[50];
+  printf("enter the string");
+  scanf("%s",s);
+  if(s[0]=='s'||s[0]=='S')
+  {
+  
+    printf("yes");}
+    else
+    {
+      printf("no");
+    }
+    return 0;
+}
